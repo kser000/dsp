555a0c09760201bb19369832eb8c843263ebf996
kser000 <1245987@gmail.com>
2019-12-03 12:45:19 +0000

    new

--- dsp_hw3/mydisambig.cpp
+++ dsp_hw3/mydisambig.cpp
@@ -1,13 +1,15 @@
 #include "Ngram.h"
 #include <stdio.h>
 #include "Vocab.h"
-#include "VocabMap.h"
+#include <map>
+#include <vector>
 #include "File.h"
 #include "Prob.h"
 #define MAX_LINE 50000
 #define MAX_LEN 200
 #define MAX_CANDIDATE 1000
 #define ZERO_P -100
+using namespace std;
 int main(int argc, char **argv){
 	int order = 2;
 	Vocab voc;
@@ -15,9 +17,22 @@ int main(int argc, char **argv){
 	FILE *outfile = fopen(argv[4],"w");
 
 	/* read map */
-	VocabMap map(Z,B);
+	//VocabMap map(Z,B);
+	map<VocabIndex,vector<VocabIndex>> m;
+
 	File mapfile(argv[2],"r");
-	map.read(mapfile);
+	char *buf;
+	while(buf = mapfile.getline()){
+		VocabString tmp[MAX_LINE];
+		unsigned int len = Vocab::parseWords(buf,tmp,MAX_LINE);
+		if(Z.getIndex(tmp[0]) == Vocab_None){
+			VocabIndex key = Z.addWord(tmp[0]); // add to Zhu-In
+			for(int i = 1;i<len;i++){
+				VocabIndex value = B.addWord(tmp[i]);
+				m[key].push_back(value);
+			}
+		}
+	}
 	mapfile.close();
 
 	/* read language model */	
@@ -30,7 +45,6 @@ int main(int argc, char **argv){
 	File testfile(argv[1],"r");
 	char *str;
 	while(str = testfile.getline()){
-		VocabIndex value; // map value
 		Prob p; // probability
 		VocabIndex empty_context[] = {Vocab_None};
 		VocabIndex bigram_context[] = {Vocab_None,Vocab_None};
@@ -39,20 +53,17 @@ int main(int argc, char **argv){
 		int Backtrack[MAX_LEN][MAX_CANDIDATE];
 		int candidate_num[MAX_LEN];
 		VocabString test[MAX_LINE];
-		unsigned int numOfwords = Vocab::parseWords(str,&(test[1]),MAX_LINE);
-		test[0] = "<s>";
-		test[numOfwords+1] = "</s>";
-		numOfwords += 2;
+		unsigned int numOfwords = Vocab::parseWords(str,test,MAX_LINE);
 		/* initialize viterbi*/
-		VocabMapIter iter(map,Z.getIndex(test[0]));
-		iter.init();
 		int numOfCan = 0;
-		while(iter.next(value,p)){
-			VocabIndex tmpCan = voc.getIndex(B.getWord(value));
+		VocabIndex Key = Z.getIndex(test[0]);
+		int len = m[Key].size();
+		for(int i = 0;i<len;i++){
+			VocabIndex tmpCan = voc.getIndex(B.getWord(m[Key][i]));
 			tmpCan = (tmpCan == Vocab_None) ? voc.getIndex(Vocab_Unknown) : tmpCan;
 			LogP tmpP = lm.wordProb(tmpCan,empty_context);
 			viterbiP[0][numOfCan] = (tmpP == LogP_Zero) ? ZERO_P : tmpP;
-			graph[0][numOfCan] = value;
+			graph[0][numOfCan] = m[Key][i];
 			Backtrack[0][numOfCan] = -1;
 			numOfCan++;
 		}
@@ -60,11 +71,11 @@ int main(int argc, char **argv){
 	
 		/* recursion of viterbi */
 		for(int i = 1;i<numOfwords;i++){
-			VocabMapIter iter(map,Z.getIndex(test[i]));
-			iter.init();
+			Key = Z.getIndex(test[i]);
 			numOfCan = 0;
-			while(iter.next(value,p)){
-				VocabIndex tmpCan = voc.getIndex(B.getWord(value));
+			len = m[Key].size();
+			for(int s = 0;s < len;s++){
+				VocabIndex tmpCan = voc.getIndex(B.getWord(m[Key][s]));
 				tmpCan = (tmpCan == Vocab_None) ? voc.getIndex(Vocab_Unknown) : tmpCan;
 				LogP maxP = LogP_Zero;
 				for(int j = 0;j<candidate_num[i-1];j++){
@@ -81,7 +92,7 @@ int main(int argc, char **argv){
 					}
 				}
 				viterbiP[i][numOfCan] = maxP;
-				graph[i][numOfCan] = value;
+				graph[i][numOfCan] = m[Key][s];
 				numOfCan++;
 			}
 			candidate_num[i] = numOfCan;
@@ -103,9 +114,11 @@ int main(int argc, char **argv){
 		}
 		
 		/* write to output */
+		fprintf(outfile,"<s> ");
 		for(int i = 0;i<numOfwords;i++){
-			fprintf(outfile,"%s%s",ans[i],(i == numOfwords-1) ? "\n":" ");
+			fprintf(outfile,"%s ",ans[i]);
 		}
+		fprintf(outfile,"</s>\n");
 	}
 	testfile.close();
 	return 0;
